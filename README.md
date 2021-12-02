a7e24bd598a996afe444562582994953a7124da4
trevyn <github@eden2.com>
2021-12-01 19:45:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# turbocharger-template
+
+Template for a fully-configured Turbocharger project with Svelte, Tailwind, and Turbosql.
+
+Run full stack:
+
+```
+npm run start-debug
+```
+
+Run Svelte frontend only in Hot Module Replacement dev mode (you'll need to manually run the server with `npm run start-debug` in a separate terminal, and re-run it if you change any Rust code):
+
+```
+npm run frontend-dev
+```
+
+Make release build:
+
+```
+npm run build
+```
---
+++ src/server.rs
@@ -0,0 +1,46 @@
+mod backend;
+
+use clap::Parser;
+
+#[derive(clap::Parser)]
+struct Opts {
+ #[clap(short, long)]
+ cert_path: Option<String>,
+ #[clap(short, long)]
+ key_path: Option<String>,
+ #[clap(short, long, default_value = "8080")]
+ port: u16,
+}
+
+#[tokio::main]
+async fn main() {
+ #[derive(rust_embed::RustEmbed)]
+ #[folder = "build"]
+ struct Frontend;
+
+ pretty_env_logger::init_timed();
+ let opts = Opts::parse();
+
+ log::warn!("warn enabled");
+ log::info!("info enabled");
+ log::debug!("debug enabled");
+ log::trace!("trace enabled");
+
+ match (opts.key_path, opts.cert_path) {
+  (Some(key_path), Some(cert_path)) => {
+   eprintln!("Serving HTTPS on port {}", opts.port);
+   warp::serve(turbocharger::warp_routes(Frontend))
+    .tls()
+    .cert_path(cert_path)
+    .key_path(key_path)
+    .run(([0, 0, 0, 0], opts.port))
+    .await;
+  }
+  (None, None) => {
+   eprintln!("Serving (unsecured) HTTP on port {}", opts.port);
+   opener::open(format!("http://127.0.0.1:{}", opts.port)).ok();
+   warp::serve(turbocharger::warp_routes(Frontend)).run(([0, 0, 0, 0], opts.port)).await;
+  }
+  _ => eprintln!("Both key-path and cert-path must be specified for HTTPS."),
+ }
+}
