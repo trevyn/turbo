d7fd2024b57d1b19cd7268bd6e26ed1c457b2df6
trevyn <trevyn-git@protonmail.com>
2021-12-09 19:46:41 +0300

    try different self_update approach

--- src/server.rs
+++ src/server.rs
@@ -28,15 +28,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
  log::debug!("debug enabled");
  log::trace!("trace enabled");
 
- let status = self_update::backends::github::Update::configure()
-  .repo_owner("trevyn")
-  .repo_name("turbo")
-  .bin_name("turbo-linux")
-  .show_download_progress(true)
-  // .current_version(cargo_crate_version!())
-  .build()?
-  .update()?;
- println!("Update status: `{}`!", status.version());
+ tokio::task::spawn_blocking(move || {
+  let result = move || -> Result<(), Box<dyn ::std::error::Error>> {
+   let releases = self_update::backends::github::ReleaseList::configure()
+    .repo_owner("trevyn")
+    .repo_name("turbo")
+    .build()
+    .unwrap()
+    .fetch()
+    .unwrap();
+   println!("found releases:");
+   println!("{:#?}\n", releases);
+
+   // get the first available release
+   let asset = releases[0].asset_for(&self_update::get_target()).unwrap();
+
+   let tmp_dir =
+    tempfile::Builder::new().prefix("self_update").tempdir_in(::std::env::current_dir()?)?;
+   let tmp_file_path = tmp_dir.path().join(&asset.name);
+   let tmp_file = ::std::fs::File::open(&tmp_file_path)?;
+
+   self_update::Download::from_url(&asset.download_url)
+    .set_header(reqwest::header::ACCEPT, "application/octet-stream".parse()?)
+    .download_to(&tmp_file)?;
+
+   Ok(())
+  }();
+
+  dbg!(result).ok();
+ })
+ .await
+ .ok();
 
  match (opts.domain, opts.key_path, opts.cert_path) {
   (Some(domain), None, None) => {
