64886921e5cf47bc1bb7df83acfb30fc6cbc127d
trevyn <trevyn-git@protonmail.com>
2022-01-23 11:00:28 +0300

    test check for updates instead

--- src-frontend/tests/turbocharger.test.js
+++ src-frontend/tests/turbocharger.test.js
@@ -3,7 +3,7 @@ import turbocharger_init, * as backend from "../turbocharger_generated";
 it("does stuff", async () => {
  await turbocharger_init();
  console.log(
-  "backend.getblockchaininfo(): ",
-  await backend.getblockchaininfo()
+  "backend.check_for_updates(): ",
+  await backend.check_for_updates()
  );
 });
