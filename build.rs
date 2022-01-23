5cb1dfae9d8b8514222809b96f1f8f88aac927c5
trevyn <trevyn-git@protonmail.com>
2022-01-23 09:54:56 +0300

    use `time` crate instead of `chrono`

--- build.rs
+++ build.rs
@@ -1,3 +1,8 @@
 fn main() {
- println!("cargo:rustc-env=BUILD_TIME={}", chrono::Utc::now().to_rfc3339());
+ println!(
+  "cargo:rustc-env=BUILD_TIME={}",
+  time::OffsetDateTime::from(std::time::SystemTime::now())
+   .format(&time::format_description::well_known::Rfc3339)
+   .unwrap()
+ );
 }
