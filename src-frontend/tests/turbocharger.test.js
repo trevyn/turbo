07c4f42d7bda3caf6ccab04aeb5b2b4e17570ef6
trevyn <trevyn-git@protonmail.com>
2022-01-23 10:48:08 +0300

    split ci build and test

--- src-frontend/tests/turbocharger.test.js
+++ src-frontend/tests/turbocharger.test.js
@@ -2,7 +2,8 @@ import turbocharger_init, * as backend from "../turbocharger_generated";
 
 it("does stuff", async () => {
  await turbocharger_init();
- let person = Object.assign(new backend.Person(), { name: "Bob" });
- let rowid = await backend.insert_person(person);
- console.log("Inserted rowid ", rowid.toString());
+ console.log(
+  "backend.getblockchaininfo(): ",
+  await backend.getblockchaininfo()
+ );
 });
