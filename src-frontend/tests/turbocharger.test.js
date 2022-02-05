337119cede468c08d0027be6ac46c282d70cf6dd
trevyn <trevyn-git@protonmail.com>
2022-02-05 15:54:45 +0300

    update for new turbocharger_test

--- src-frontend/tests/turbocharger.test.js
+++ src-frontend/tests/turbocharger.test.js
@@ -2,5 +2,6 @@ import turbocharger_init, * as backend from "../turbocharger_generated";
 
 it("does stuff", async () => {
  await turbocharger_init();
+ backend.set_socket_url("ws://localhost:8080/turbocharger_socket");
  console.log("backend.heartbeat(): ", await backend.heartbeat());
 });
