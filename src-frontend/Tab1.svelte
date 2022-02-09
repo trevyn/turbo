676c99f60061ae65b4fb5fcdf98fa602e3fcc091
trevyn <trevyn-git@protonmail.com>
2022-02-09 18:11:34 +0300

    fix flash of undefined

--- src-frontend/Tab1.svelte
+++ src-frontend/Tab1.svelte
@@ -4,6 +4,4 @@
  // let foo = animal_time_stream.subscribe();
 </script>
 
-<p class="text-3xl font-bold text-gray-400">{$animal_time_stream}</p>
-
-<p class="text-3xl font-bold text-gray-400">{$animal_time_stream}</p>
+<p class="text-3xl font-bold text-gray-400">{$animal_time_stream || ""}</p>
