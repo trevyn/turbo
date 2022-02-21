63e5a094f96cb7c04641d6d79b5a482d3352714c
trevyn <trevyn-git@protonmail.com>
2022-02-21 06:15:44 +0300

    rerun build script less often

--- build.rs
+++ build.rs
@@ -1,4 +1,7 @@
 fn main() {
+ // Opt in to "new style" fingerprinting by setting a rerun-if-* value:
+ // https://github.com/rust-lang/cargo/blob/46c9b51957e335ad03051ae8c513f3ba2a55b3b2/src/cargo/core/compiler/fingerprint.rs#L267-L276
+ println!("cargo:rerun-if-env-changed=BUILD_ADJECTIVE");
  if option_env!("BUILD_ADJECTIVE").is_none() {
   println!(
    "cargo:rustc-env=BUILD_TIME={}",
