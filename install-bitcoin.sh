cadbd76dfa237ec87140338bbd21c98431a77629
trevyn <trevyn-git@protonmail.com>
2022-02-22 17:09:28 +0300

    polish

--- install-bitcoin.sh
+++ install-bitcoin.sh
@@ -1,5 +1,5 @@
 scp bitcoin.tar.gz root@test4.turbonet.to:
-wget https://bitcoin.org/bin/bitcoin-core-22.0/bitcoin-22.0-x86_64-linux-gnu.tar.gz
+wget https://bitcoincore.org/bin/bitcoin-core-22.0/bitcoin-22.0-x86_64-linux-gnu.tar.gz
 tar xvf bitcoin.tar.gz
 tar xvf bitcoin-22.0-x86_64-linux-gnu.tar.gz
 cd bitcoin-22.0/bin
