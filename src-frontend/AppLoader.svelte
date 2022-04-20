2d4ea61851d001cb199b1312a1a347753c2cfd1d
trevyn <230691+trevyn@users.noreply.github.com>
2022-04-20 12:04:56 +0300

    reorder turbo_start_web

--- src-frontend/AppLoader.svelte
+++ src-frontend/AppLoader.svelte
@@ -3,13 +3,13 @@
  import App from "./App.svelte";
 
  let prom = turbocharger_init().then(async () => {
-  backend.turbo_start_web();
   if (
    window.location.href == "http://127.0.0.1:3000/" ||
    window.location.href == "http://localhost:3000/"
   )
    backend.set_socket_url("ws://127.0.0.1:8080/turbocharger_socket");
   await backend.wasm_notify_client_pk();
+  backend.turbo_start_web();
  });
 </script>
 
