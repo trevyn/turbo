754db3ed595b630a59df2f07e41043e9ed1ce507
trevyn <230691+trevyn@users.noreply.github.com>
2022-04-28 13:53:44 +0400

    add do_backend, do_backend_stream

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
-use arc_swap::{ArcSwap, ArcSwapOption};
-use eframe::{egui, epaint::Vec2};
-use std::sync::{Arc, Mutex};
-use turbocharger::futures_util::StreamExt;
+use arc_swap::ArcSwapOption;
+use eframe::egui;
+use std::sync::Arc;
+use turbocharger::futures_util::{self, StreamExt};
 use turbocharger::prelude::*;
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
@@ -9,28 +9,54 @@ use turbocharger::prelude::*;
 #[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
 #[derive(Default)]
 pub struct TemplateApp {
- // Example stuff:
  label: String,
-
- // this how you opt-out of serialization of a member
  #[cfg_attr(feature = "persistence", serde(skip))]
  value: f32,
-
- encrypted_animal_time_stream: Arc<Mutex<Vec<u8>>>,
-
- mail_list: Arc<ArcSwap<Vec<crate::backend::mail>>>,
-
+ encrypted_animal_time_stream: Arc<ArcSwapOption<String>>,
+ mail_list: Arc<ArcSwapOption<Vec<String>>>,
  check_for_updates: Arc<ArcSwapOption<Result<String, tracked::StringError>>>,
-
  selected_anchor: String,
+ num_frames: u32,
+}
+
+fn do_backend<T: 'static>(
+ ctx: &egui::Context,
+ store: &Arc<ArcSwapOption<T>>,
+ fut: impl std::future::Future<Output = T> + 'static,
+) {
+ let ctx = ctx.clone();
+ let store = store.clone();
+ wasm_bindgen_futures::spawn_local(async move {
+  store.store(Some(Arc::new(fut.await)));
+  ctx.request_repaint();
+ });
+}
+
+fn do_backend_stream<T: 'static, U>(
+ ctx: &egui::Context,
+ store: &Arc<ArcSwapOption<T>>,
+ stream: impl futures_util::stream::Stream<Item = U> + 'static,
+ transform: impl Fn(U) -> T + 'static,
+) {
+ let ctx = ctx.clone();
+ let store = store.clone();
+ let mut stream = Box::pin(stream);
+ wasm_bindgen_futures::spawn_local(async move {
+  while let Some(item) = stream.next().await {
+   store.store(Some(Arc::new(transform(item))));
+   ctx.request_repaint();
+  }
+ });
 }
 
 impl TemplateApp {
  pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-  // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
-  // Restore app state using cc.storage (requires the "persistence" feature).
-  // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
-  // for e.g. egui::PaintCallback.
+  let ctx = &cc.egui_ctx;
+  let mut style = ctx.style().as_ref().clone();
+  style.visuals.widgets.noninteractive.fg_stroke.color = egui::Color32::WHITE;
+  style.visuals.widgets.active.fg_stroke.color = egui::Color32::WHITE;
+  style.visuals.widgets.inactive.fg_stroke.color = egui::Color32::WHITE;
+  ctx.set_style(style);
 
   // Load previous app state (if any).
   // Note that you must enable the `persistence` feature for this to work.
@@ -39,37 +65,34 @@ impl TemplateApp {
   //  *self = epi::get_value(storage, epi::APP_KEY).unwrap_or_default()
   // }
 
-  let mut style = (*cc.egui_ctx.style()).clone();
-  style.visuals.widgets.noninteractive.fg_stroke.color = egui::Color32::WHITE;
-  style.visuals.widgets.active.fg_stroke.color = egui::Color32::WHITE;
-  style.visuals.widgets.inactive.fg_stroke.color = egui::Color32::WHITE;
-  cc.egui_ctx.set_style(style);
-
   let s = Self::default();
 
-  let mut stream = Box::pin(crate::backend::encrypted_animal_time_stream());
-  let encrypted_animal_time_stream = s.encrypted_animal_time_stream.clone();
-  let ctx = cc.egui_ctx.clone();
-  wasm_bindgen_futures::spawn_local(async move {
-   while let Some(item) = stream.next().await {
-    *encrypted_animal_time_stream.lock().unwrap() = item.unwrap();
-    ctx.request_repaint();
-   }
+  do_backend(ctx, &s.check_for_updates, crate::backend::check_for_updates());
+
+  do_backend(ctx, &s.mail_list, async {
+   crate::backend::mail_list()
+    .await
+    .unwrap()
+    .into_iter()
+    .map(|mail| {
+     let decrypted = crate::wasm_decrypt(mail.data.unwrap()).unwrap();
+     let parsed = mailparse::parse_mail(&decrypted.as_bytes()).unwrap();
+     let body = parsed
+      .subparts
+      .iter()
+      .find(|subpart| subpart.ctype.mimetype == "text/plain")
+      .map(|subpart| subpart.get_body().unwrap());
+     body.unwrap_or_default()
+    })
+    .collect()
   });
 
-  let ctx = cc.egui_ctx.clone();
-  let mail_list = s.mail_list.clone();
-  wasm_bindgen_futures::spawn_local(async move {
-   mail_list.store(Arc::new(crate::backend::mail_list().await.unwrap()));
-   ctx.request_repaint();
-  });
-
-  let ctx = cc.egui_ctx.clone();
-  let check_for_updates = s.check_for_updates.clone();
-  wasm_bindgen_futures::spawn_local(async move {
-   check_for_updates.store(Some(Arc::new(crate::backend::check_for_updates().await)));
-   ctx.request_repaint();
-  });
+  do_backend_stream(
+   ctx,
+   &s.encrypted_animal_time_stream,
+   crate::backend::encrypted_animal_time_stream(),
+   |r| crate::wasm_decrypt(r.unwrap()).unwrap_or("decrypt error".into()),
+  );
 
   s
  }
@@ -81,21 +104,14 @@ impl eframe::App for TemplateApp {
  // }
 
  /// Called by the frame work to save state before shutdown.
- /// Note that you must enable the `persistence` feature for this to work.
  #[cfg(feature = "persistence")]
  fn save(&mut self, storage: &mut dyn epi::Storage) {
   epi::set_value(storage, epi::APP_KEY, self);
  }
 
- /// Called each time the UI needs repainting, which may be many times per second.
  /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
- fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-  let Self { label, value, encrypted_animal_time_stream, .. } = self;
-
-  // Examples of how to create different panels and windows.
-  // Pick whichever suits you.
-  // Tip: a good default choice is to just keep the `CentralPanel`.
-  // For inspiration and more examples, go to https://emilk.github.io/egui
+ fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+  let Self { label, value, .. } = self;
 
   egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
    ui.horizontal_wrapped(|ui| {
@@ -109,7 +125,7 @@ impl eframe::App for TemplateApp {
     {
      let label = ui.selectable_label(
       self.selected_anchor == anchor,
-      egui::RichText::new(name).font(egui::FontId::proportional(25.0)),
+      egui::RichText::new(name).font(egui::FontId::proportional(20.0)),
      );
      if label.hovered() {
       ui.output().cursor_icon = egui::CursorIcon::PointingHand;
@@ -134,6 +150,10 @@ impl eframe::App for TemplateApp {
     *value += 1.0;
    }
 
+   self.num_frames += 1;
+   ui.label(format!("num_frames: {}", self.num_frames));
+   ui.label(format!("last frame: {:.0} ms", frame.info().cpu_usage.unwrap_or_default() * 1000.0));
+
    egui::warn_if_debug_build(ui);
   });
 
@@ -144,42 +164,32 @@ impl eframe::App for TemplateApp {
    size.y = 0.0;
 
    egui::ScrollArea::vertical().show(ui, |ui| {
-    let text = format!("check_for_updates: {:?}", self.check_for_updates.load());
-
-    if ui
-     .add_sized(
-      size,
-      egui::TextEdit::multiline(&mut text.as_str()).font(egui::FontId::proportional(15.0)),
+    ui.add_sized(
+     size,
+     egui::TextEdit::multiline(
+      &mut format!("check_for_updates: {:?}", self.check_for_updates.load()).as_str(),
      )
-     .hovered()
-    {
-     ui.output().cursor_icon = egui::CursorIcon::Default;
-    };
+     .font(egui::FontId::proportional(15.0))
+     .desired_rows(2),
+    );
 
-    let text = crate::wasm_decrypt((*encrypted_animal_time_stream.lock().unwrap()).clone())
-     .unwrap_or_default();
-
-    if ui
-     .add_sized(
+    if let Some(encrypted_animal_time) = self.encrypted_animal_time_stream.load().as_deref() {
+     ui.add_sized(
       size,
-      egui::TextEdit::multiline(&mut text.as_str()).font(egui::FontId::proportional(15.0)),
-     )
-     .hovered()
-    {
-     ui.output().cursor_icon = egui::CursorIcon::Default;
-    };
+      egui::TextEdit::multiline(&mut encrypted_animal_time.as_str())
+       .font(egui::FontId::proportional(15.0))
+       .desired_rows(2),
+     );
+    }
 
-    for mail in self.mail_list.load().iter() {
-     let text = crate::wasm_decrypt(mail.data.clone().unwrap()).unwrap();
-     if ui
-      .add_sized(
+    // for mail in self.mail_list.load().iter() {
+    if let Some(mail_list) = self.mail_list.load().as_deref() {
+     if let Some(mail) = mail_list.first() {
+      ui.add_sized(
        size,
-       egui::TextEdit::multiline(&mut text.as_str()).font(egui::FontId::proportional(15.0)),
-      )
-      .hovered()
-     {
-      ui.output().cursor_icon = egui::CursorIcon::Default;
-     };
+       egui::TextEdit::multiline(&mut mail.as_str()).font(egui::FontId::proportional(15.0)),
+      );
+     }
     }
    });
   });
