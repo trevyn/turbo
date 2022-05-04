005220a614eed93e221ad4760cf496745f52e06a
trevyn <230691+trevyn@users.noreply.github.com>
2022-05-04 07:57:30 +0400

    add *.rs to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
 module.exports = {
  mode: "jit",
- content: ["./src-frontend/**/*.{html,js,jsx,ts,tsx,svelte}"],
+ content: ["./src-frontend/**/*.{html,js,jsx,ts,tsx,svelte}", "./src/**/*.rs"],
 };
