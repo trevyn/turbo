502ebb0764a289cd2c88bad82e298c4081b1257a
trevyn <230691+trevyn@users.noreply.github.com>
2022-07-22 21:27:22 -0400

    fix localhost detection

--- src-frontend/no-svelte.js
+++ src-frontend/no-svelte.js
@@ -2,8 +2,8 @@ import turbocharger_init, * as backend from "./turbocharger_generated";
 
 let prom = turbocharger_init().then(async () => {
  if (
-  window.location.href == "http://127.0.0.1:3000/" ||
-  window.location.href == "http://localhost:3000/"
+  window.location.href.startsWith("http://127.0.0.1:3000/") ||
+  window.location.href.startsWith("http://localhost:3000/")
  )
   backend.set_socket_url("ws://127.0.0.1:8080/turbocharger_socket");
  // await backend.wasm_notify_client_pk();
