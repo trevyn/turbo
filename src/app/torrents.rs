2079732c75fee3a5e6b6055481643c5f934c54e1
trevyn <230691+trevyn@users.noreply.github.com>
2022-10-04 15:56:00 +0400

    extract infohash with regex

--- src/app/torrents.rs
+++ src/app/torrents.rs
@@ -4,8 +4,6 @@ use super::*;
 pub fn Torrents(cx: Scope) -> Element {
  let results = use_state(&cx, || None);
 
- let torrent_url = "magnet:?xt=urn:btih:dd8255ecdc7ca55fb0bbf81323d87062db1f6d1c&dn=Big+Buck+Bunny&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fbig-buck-bunny.torrent";
-
  rsx!(cx, p {
   ActionButton{action: auth_password, "Submit Password"}
   ActionButton{action: do_test_action, "Do Test Action"}
@@ -13,7 +11,8 @@ pub fn Torrents(cx: Scope) -> Element {
   ActionButton{action: launch_jackett, "Launch Jackett"}
   ActionButton{action: configure_jackett, "Configure Jackett"}
   ActionButton{action: search_jackett, results: results, "Search Jackett"}
-  ActionButton{action: move || rqbit_do_torrent(torrent_url.into(), "bigbuckbunny".into()), "download torrent"}
+  ActionButton{action: move |s| rqbit_do_torrent(s, "direct-magnets".into()), "download magnet url"}
+
   TorrentTable{results: results}
  })
 }
@@ -41,7 +40,8 @@ pub fn TorrentTable<'a>(cx: Scope<'a, TorrentTableProps<'a>>) -> Element<'a> {
       let size = r.size.unwrap()/(1024*1024);
       let title = r.title.clone().unwrap();
 
-      let infohash = r.guid.as_ref().unwrap().strip_prefix("https://rarbg.to/infohash/").unwrap();
+      let infohash = regex::Regex::new("([0-9a-fA-F]{40})").unwrap().captures(r.guid.as_ref().unwrap()).unwrap().get(1).unwrap().as_str();
+
       let torrent_url = format!("magnet:?xt=urn:btih:{}&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz", infohash);
 
       rsx!(
@@ -49,9 +49,9 @@ pub fn TorrentTable<'a>(cx: Scope<'a, TorrentTableProps<'a>>) -> Element<'a> {
        td { "{title}" }
        td { "{seeders}" }
        td { "{size} MB" }
+       td { "{infohash}" }
 
-       td { ActionButton{action: move || rqbit_do_torrent(torrent_url.clone(), title.clone()), "download torrent"}
-      }
+       td { ActionButton{action: move || rqbit_do_torrent(torrent_url.clone(), title.clone()), "download torrent"} }
       }
      )})
     }
